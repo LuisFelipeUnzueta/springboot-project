fd43564fd679162a92ac84d3a508eb7e3c64d6d7
Luis Unzueta <luis.unzueta@bindflow.com.br>
2024-01-07 08:07:31 -0300

    Service layer, component registration

--- src/main/java/com/luisunzueta/cursojava/resources/UserResources.java
+++ src/main/java/com/luisunzueta/cursojava/resources/UserResources.java
@@ -1,17 +1,29 @@
 package com.luisunzueta.cursojava.resources;
 
 import com.luisunzueta.cursojava.entities.User;
+import com.luisunzueta.cursojava.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
+    @Autowired
+    public UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"Luis Felipe", "lfunzueta@gmail.com", "41997555317", "teste123");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
---
+++ src/main/java/com/luisunzueta/cursojava/services/UserService.java
@@ -0,0 +1,24 @@
+package com.luisunzueta.cursojava.services;
+
+import com.luisunzueta.cursojava.entities.User;
+import java.util.List;
+import java.util.Optional;
+
+import com.luisunzueta.cursojava.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
